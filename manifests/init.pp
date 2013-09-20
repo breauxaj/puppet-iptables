9a2f55510a1cb41f2a7060009eeb47cd0be3d2f3
Adam Breaux <breauxaj@gmail.com>
2013-09-19 20:37:35 -0400

    puppet lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,45 +1,8 @@
 class iptables {
-  $required = $operatingsystem ? {
+  $required = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'iptables' ],
   }
 
   package { $required: ensure => latest }
-
-}
-
-define iptables::service ( $ensure,
-                           $enable ) {
-  $service = $operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => 'iptables',
-  }
-
-  service { $service:
-    ensure => $ensure,
-    enable => $enable,
-  }
-
-}
-
-define iptables::sysconfig ( $modules,
-                             $modules_unload = 'yes',
-                             $save_on_stop = 'no',
-                             $save_on_restart = 'no',
-                             $save_counter = 'no',
-                             $status_numeric = 'yes',
-                             $status_verbose = 'no',
-                             $status_linenumbers = 'yes',
-                             $sysctl_load_list = [ '.nf_conntrack', '.bridge-nf' ] ) {
-  file { '/etc/sysconfig/iptables-config':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-    content => template('iptables/iptables-config.erb'),
-  }
-
-}
-
-define iptables::rule () {
-
 
 }
